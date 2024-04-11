46f4d469567b14d87c91c27d7665410b22a29bb8
myhre062 <myhre062@umn.edu>
2024-04-11 18:11:23 -0500

    partial attempt

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,30 +1,20 @@
 // This data is updated every minute
-const earthquakeDataUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+let earthquakeDataUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Create a Leaflet map instance
-var map = L.map('map').setView([0, 0], 2); // Set initial view at center of the world with zoom level 2
+console.log(sum)
+d3.json(earthquakeDataUrl).then(function(response) {
 
-// Add a base layer to the map
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    console.log(response);
+});
 
-// Fetch earthquake data
-fetch(earthquakeDataUrl)
-  .then(response => response.json())
-  .then(data => {
-    // Iterate over the earthquake data
-    data.features.forEach(feature => {
-      // Extract coordinates
-      const coords = feature.geometry.coordinates;
-      const lat = coords[1];
-      const lon = coords[0];
+// // Create earthquake layerGroup
+var earthquakes = L.layerGroup();
 
-      // Create marker and add to map
-      L.marker([lat, lon]).addTo(map)
-        .bindPopup(`<b>${feature.properties.place}</b><br>Magnitude: ${feature.properties.mag}`);
-    });
-  })
-  .catch(error => {
-    console.error('Error fetching earthquake data:', error);
-  });
+// Create tile layer
+var grayscaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}", {
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+});
+
+// Can't make this work. This is the best I got. 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # leaflet-challenge
+
+I couldn't make this work so I prioritized project 3 work and will circle back around to this project. Willing to take the minimum score I can take until I get it regraded. 
